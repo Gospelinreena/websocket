17c6b0a70400640558cbb4559c1aa51919ce7f6c
Gospelinreena <gospelinreena@gmail.com>
2026-03-17 15:39:51 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,27 @@
+import asyncio
+import websockets
+async def client():
+    uri = "ws://localhost:8765"
+
+    async with websockets.connect(uri) as websocket:
+        username = input("Enter username: ")
+        await websocket.send(username)
+
+        async def send():
+            loop = asyncio.get_event_loop()
+            while True:
+                msg = await loop.run_in_executor(None, input)
+                await websocket.send(msg)
+
+        async def receive():
+            while True:
+                try:
+                    response = await websocket.recv()
+                    print("\n" + response)
+                except websockets.exceptions.ConnectionClosed:
+                    print("Disconnected")
+                    break
+
+        await asyncio.gather(send(), receive())
+
+asyncio.run(client())
---
+++ server.py
@@ -0,0 +1,58 @@
+import asyncio
+import websockets
+connected_clients = {}
+
+async def server(websocket):
+    username = None
+    try:
+        username = await websocket.recv()
+
+        if username in connected_clients:
+            await websocket.send("Username already taken. Try another.")
+            return
+
+        connected_clients[username] = websocket
+
+        print(f"{username} connected")
+        print("Total clients:", len(connected_clients))
+
+        await asyncio.gather(
+            *[
+                client.send(f"{username} joined")
+                for user, client in connected_clients.items()
+                if client != websocket
+            ]
+        )
+
+        async for message in websocket:
+            print(f"{username}: {message}")
+
+            await asyncio.gather(
+                *[
+                    client.send(f"{username}: {message}")
+                    for user, client in connected_clients.items()
+                    if client != websocket
+                ]
+            )
+    except websockets.exceptions.ConnectionClosed:
+        print(f"{username} disconnected")
+
+    finally:
+        if username and username in connected_clients:
+            del connected_clients[username]
+
+            print(f"{username} removed")
+            print("Total clients:", len(connected_clients))
+
+            if connected_clients:
+                await asyncio.gather(
+                    *[
+                        client.send(f"{username} left")
+                        for client in connected_clients.values()
+                    ]
+                )
+async def main():
+    async with websockets.serve(server, "localhost", 8765):
+        print("Server started")
+        await asyncio.Future()
+asyncio.run(main())
